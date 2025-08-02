92d6e7258c91bfe2cef3e9fde183e8f0d36f715d
Rishabh Mishra <mrishabh845@gmail.com>
2025-08-02 22:24:42 +0530

    change port

--- src/app.js
+++ src/app.js
@@ -38,6 +38,7 @@ app.use('/user',userRouter);
 connectDB();
 
 // Start the server
-app.listen(3000, () => {
-  console.log("🚀 Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
