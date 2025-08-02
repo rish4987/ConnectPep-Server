d62fa7e7dd134159f4b2b913d6a5e04c38c0a0a2
Rishabh Mishra <mrishabh845@gmail.com>
2025-08-02 22:52:32 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -16,10 +16,12 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 dotenv.config();
-app.use(cors({
-  origin: ['http://16.171.199.229', 'http://localhost:5173'], 
-  credentials: true
-}));
+app.use(cors(
+  {
+    origin:'http://localhost:5173',
+    credentials: true
+  }
+))
  
 const JWT_SECRET = process.env.JWT_SECRET || 'Dev@tinder$12323'; 
 
@@ -28,7 +30,8 @@ const JWT_SECRET = process.env.JWT_SECRET || 'Dev@tinder$12323';
 app.use('/auth', authRouter);
 app.use('/profile', profileRouter);
 app.use('/requests', requestRouter);
-app.use('/user',userRouter);
+app.use('/user',userRouter)
+
 
 
 
@@ -38,7 +41,6 @@ app.use('/user',userRouter);
 connectDB();
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on http://0.0.0.0:${PORT}`);
+app.listen(3000, () => {
+  console.log("🚀 Server running on http://localhost:3000");
 });
